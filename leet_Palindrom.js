99f42704e092f6d45941708b0a562dcddfc14173
muratirmak <mrtriver@gmail.com>
2022-12-10 13:23:34 -0500

    palindrome updated

--- leet_Palindrom.js
+++ leet_Palindrom.js
@@ -1,10 +1,23 @@
-var isPalindrome = function(x) {
-    if (x < 0) return false;
+// var isPalindrome = function(x) {
+//     if (x < 0) return false;
 	
-    // reverse the string representation of x
-    const reverse = `${x}`.split('').reverse().join('');
-    // compare the value regardless of types
-    return x == reverse;
-};
+//     // reverse the string representation of x
+//     const reverse = `${x}`.split('').reverse().join('');
+//     // compare the value regardless of types
+//     return x == reverse;
+// };
 
-console.log(isPalindrome(191));
+// console.log(isPalindrome(191));
+
+var isPalindrome = function(x) { 
+    let numGiven =x;
+let rev = 0;
+while(x !== 0){
+    let n = x % 10;
+    rev = rev * 10 + n;
+    x = Math.floor(x / 10);
+}
+return rev == numGiven;
+}
+
+console.log(isPalindrome(0));
